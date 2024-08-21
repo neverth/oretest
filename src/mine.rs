48ee6a307c0ab540764de592cb1dbec2f6b75bf1
liyang.112 <liyang.112@bytedance.com>
2024-08-21 12:27:29 +0800

    add jq6

--- src/mine.rs
+++ src/mine.rs
@@ -104,24 +104,12 @@ impl Miner {
                     cutoff_time, 12, config.min_difficulty, proof.challenge, 44, 16), cutoff_time + 5,
                 ),
                 fetch_data(&client, &format!(
-                    "http://77.237.247.180:6789/ore?cutoff_time={}&threads={}&min_difficulty={}&challenge={:?}&total_div={}&start_idx={}",
-                    cutoff_time, 4, config.min_difficulty, proof.challenge, 44, 28), cutoff_time + 5,
-                ),
-                fetch_data(&client, &format!(
                     "http://45.159.228.77:6789/ore?cutoff_time={}&threads={}&min_difficulty={}&challenge={:?}&total_div={}&start_idx={}",
                     cutoff_time, 4, config.min_difficulty, proof.challenge, 44, 32), cutoff_time + 5,
                 ),
                 fetch_data(&client, &format!(
                     "http://45.159.229.105:6789/ore?cutoff_time={}&threads={}&min_difficulty={}&challenge={:?}&total_div={}&start_idx={}",
                     cutoff_time, 4, config.min_difficulty, proof.challenge, 44, 36), cutoff_time + 5,
-                ),
-                fetch_data(&client, &format!(
-                    "http://39.96.168.240:6789/ore?cutoff_time={}&threads={}&min_difficulty={}&challenge={:?}&total_div={}&start_idx={}",
-                    cutoff_time, 2, config.min_difficulty, proof.challenge, 44, 40), cutoff_time + 5,
-                ),
-                fetch_data(&client, &format!(
-                    "http://60.205.146.207:6789/ore?cutoff_time={}&threads={}&min_difficulty={}&challenge={:?}&total_div={}&start_idx={}",
-                    cutoff_time, 2, config.min_difficulty, proof.challenge, 44, 42), cutoff_time + 5,
                 ),
             ])
                 .await;
